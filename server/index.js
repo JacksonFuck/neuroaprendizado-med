0ef55dfd0a0d8b9c9fade8608f15c41c8008833d
Dr. Jackson Fuck <jackson@umuarama.med.br>
2026-02-25 03:09:25 -0300

    fix: proxy: true

--- server/index.js
+++ server/index.js
@@ -53,7 +53,8 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    proxy: true
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         const { rows } = await pool.query(
